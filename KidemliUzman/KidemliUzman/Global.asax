2529871b9289f6c814fc97a38bf26f714c39bb09
DESKTOP-DEC0EEN\Salih <salihgungor13@hotmail.com>
2017-07-31 14:58:44 +0300

    front-end'te düzenlemeler yapıldı.

---
+++ KidemliUzman/KidemliUzman/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KidemliUzman.MvcApplication" Language="C#" %>
---
+++ KidemliUzman/KidemliUzman/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KidemliUzman.Startup))]
+namespace KidemliUzman
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
